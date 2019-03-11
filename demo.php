75508f3c5481615f54545b19e0f647ac815a0290
test <test@gmail.com>
2019-03-11 10:43:47 +0530

    lol

--- demo.php
+++ demo.php
@@ -7,11 +7,4 @@ echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'; ?><br/>
 						
 						
-<ul>
-<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
 
-</ul>
-<button class="btn form-btn" data-toggle="modal" data-target="#modalContactForm">Enquire Now !</button>
